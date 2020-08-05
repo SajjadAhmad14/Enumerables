0de41403fc80e21f6a58835719f34b14e67f1d7c
tadeuasarro <tadeutas@gmail.com>
2020-08-05 16:51:10 -0300

    Work on my_each_with_index

--- enum.rb
+++ enum.rb
@@ -1,11 +1,37 @@
-module Enumerable  
+module Enumerable
+  #MY EACH METHOD
   def my_each
     for value in self
       yield value
     end
   end
 
-  ar = {"sajjad": 1, "Tadue": 2}
-  ar.my_each {|n| puts n}
-
+  #MY EACH WITH INDEX METHOD
+  def my_each_with_index
+    case self
+    when Hash
+      j = 0
+      for i in self
+        yield i, j
+        j += 1
+      end
+    else
+      self.length.times do |i|
+        yield self[i], i
+      end
+    end
+  end
 end
+
+#DECLARE VARIABLES AND CALL FUNCTIONS BELOW
+array_int = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+array_str = %w[tadeu sajjad brazil pakistan microverse ruby]
+hash_first = {
+  :keyone => 'firstvalue',
+  :keytwo => 'secondvalue',
+  :keythree => 'thirdvalue',
+  :keyfour => 'fourthvalue'
+}
+
+array_int.each_with_index {|(x), z| puts "Value: #{x}, #{z}."}
+array_int.my_each_with_index {|(x), z| puts "Value: #{x}, #{z}."}
