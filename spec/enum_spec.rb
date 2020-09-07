0ef9c78fba2804d198a6e91edf794bed54e114f9
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-07 18:11:43 +0500

    Add spec test for my_select

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -1,7 +1,7 @@
 require '../enum.rb'
 
 describe Enumerable do
-  let (:array) {[1, 2, 3]}
+  let (:array) {[1, 2, 3, 4]}
   describe '#my_each' do
     it "return each element of an array" do
       expect(array.my_each{|element|}).to eql(array.my_each{|element|})
@@ -11,6 +11,12 @@ describe Enumerable do
   describe '#my_each_with_index' do
     it "return each element and it corresponding index" do
       expect(array.my_each_with_index{|element, index|}).to eql(array.my_each_with_index{|element, index|})
+    end
+  end
+
+  describe '#my_select' do
+    it "return each element for which condition is true" do
+      expect(array.my_select{|element| element % 2 == 0}).to eql([2, 4])
     end
   end
 
